57bc1b3939e7089f6d28ae97bd280c5271124e24
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-09-03 00:34:03 +0530

    added mapping configuration

--- src/main/java/com/AutoTrack/service/ResidentService.java
+++ src/main/java/com/AutoTrack/service/ResidentService.java
@@ -2,9 +2,12 @@ package com.AutoTrack.service;
 
 import com.AutoTrack.entity.Resident;
 import com.AutoTrack.repository.ResidentRepo;
+import com.AutoTrack.repository.VehiclrRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 @Component
@@ -14,8 +17,19 @@ public class ResidentService {
     @Autowired
     private ResidentRepo residentRepo;
 
-    // to save Resident data
+    @Autowired
+    private VehiclrRepo vehiclrRepo;
+
+
+
+    // to save Resident data with vehicles
     public Resident saveResident(Resident resident) {
+
+        resident.getVehicles().forEach( v -> {
+            v.setResident(resident);
+            v.setIntime(LocalDateTime.now());
+        } );
+
         return residentRepo.save(resident);
 
     }
