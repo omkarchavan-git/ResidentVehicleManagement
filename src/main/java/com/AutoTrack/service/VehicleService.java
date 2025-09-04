6dce38a7a139e28e1c6c6421f3a23d9a7333dc13
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-09-04 14:27:11 +0530

    VEHMS-M01-T015- Add vehicles with resident

---
+++ src/main/java/com/AutoTrack/service/VehicleService.java
@@ -0,0 +1,42 @@
+package com.AutoTrack.service;
+
+import com.AutoTrack.eNum.VehicleType;
+import com.AutoTrack.entity.Resident;
+import com.AutoTrack.entity.Vehicles;
+import com.AutoTrack.repository.ResidentRepo;
+import com.AutoTrack.repository.VehiclrRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class VehicleService {
+
+    @Autowired
+    private VehiclrRepo vehiclrRepo;
+
+    @Autowired
+    private ResidentRepo residentRepo;
+
+    // method to add vehicle with resident
+    public Vehicles createVehicle (Vehicles vehicles, int residentId)
+    {
+        // check if resident  exist
+
+        Resident resident =  residentRepo.findById(residentId)
+                .orElseThrow( () -> new IllegalArgumentException("Resident not found with id : " + residentId));
+
+        // validate reg num
+        if (vehicles.getRegNum() == null || vehicles.getRegNum().trim().isEmpty())
+        {
+            throw new IllegalArgumentException("Vehicle reg num is not valid");
+        }
+
+        // set resident to vehicle
+        vehicles.setResident(resident);
+        vehicles.setIntime(LocalDateTime.now().withNano(0));
+
+        return vehiclrRepo.save(vehicles);
+    }
+}
