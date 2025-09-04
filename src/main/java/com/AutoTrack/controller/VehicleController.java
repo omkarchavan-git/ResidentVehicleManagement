0025b1287949d3b9147d33847cd828a3651c62e0
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-09-04 14:26:57 +0530

    VEHMS-M01-T015- Add vehicles with resident

---
+++ src/main/java/com/AutoTrack/controller/VehicleController.java
@@ -0,0 +1,32 @@
+package com.AutoTrack.controller;
+
+import com.AutoTrack.entity.Vehicles;
+import com.AutoTrack.service.VehicleService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/vehicle")
+public class VehicleController {
+
+    @Autowired
+    private VehicleService vehicleService;
+
+    // API to add vehicle data with resident
+    @PostMapping("/addVehicle")
+    public ResponseEntity<?> addVehicle (@Valid @RequestBody Vehicles vehicles,
+                                         @RequestParam int residentId)
+    {
+        try {
+            Vehicles savedVehicle = vehicleService.createVehicle(vehicles, residentId);
+            return new ResponseEntity<>(savedVehicle, HttpStatus.CREATED);
+        } catch (IllegalArgumentException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+
+}
