b1a599c10bf31b601d8c9d1b041f144a0530d15e
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-09-04 12:09:50 +0530

    VEHMS-M01-T012- find user With Validation

--- src/main/java/com/AutoTrack/repository/ResidentRepo.java
+++ src/main/java/com/AutoTrack/repository/ResidentRepo.java
@@ -3,5 +3,13 @@ package com.AutoTrack.repository;
 import com.AutoTrack.entity.Resident;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ResidentRepo extends JpaRepository<Resident, Integer> {
+
+    List<Resident> findByFirstnameIgnoreCase(String firstname);
+
+    List<Resident> findByLastnameIgnoreCase(String lastname);
+
+    List<Resident> findByFirstnameIgnoreCaseAndLastnameIgnoreCase(String firstname, String lastname);
 }
