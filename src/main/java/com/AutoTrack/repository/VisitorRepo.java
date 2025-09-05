ffca365243c9357618431d71eb3c72054be6e92a
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-09-05 15:08:22 +0530

    Updated Visitor Repo

--- src/main/java/com/AutoTrack/repository/VisitorRepo.java
+++ src/main/java/com/AutoTrack/repository/VisitorRepo.java
@@ -3,6 +3,8 @@ package com.AutoTrack.repository;
 import com.AutoTrack.entity.Visitor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface VisitorRepo extends JpaRepository<Visitor, Integer> {
+import java.util.Optional;
 
+public interface VisitorRepo extends JpaRepository<Visitor, Integer> {
+        Optional<Visitor> findByVehicalRegisterationNumber(String regNum);
 }
