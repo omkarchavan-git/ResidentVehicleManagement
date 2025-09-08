4c14d3258e3747d8ead3163201ed56ef4ffc867d
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-09-08 16:07:29 +0530

    updated Resident Controller

---
+++ src/main/java/com/AutoTrack/controller/ResidentController.java
@@ -0,0 +1,57 @@
+package com.AutoTrack.controller;
+
+import com.AutoTrack.entity.Resident;
+import com.AutoTrack.service.ResidentService;
+
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/Resident")
+public class ResidentController {
+
+    @Autowired
+    private ResidentService residentService;
+
+
+    // add resident with vehicles
+    @PostMapping("/saveResidents")
+    public ResponseEntity<?> addResident(@Valid @RequestBody Resident resident) {
+         residentService.saveResident(resident);
+        return new ResponseEntity<>("saved Resident", HttpStatus.CREATED);
+    }
+
+    // API to get all resident
+    @GetMapping("/getAllResident")
+    public ResponseEntity<List<Resident>> getallresidents () {
+        List<Resident> residentList = residentService.getAllDevelopers();
+        return new ResponseEntity<>(residentList,HttpStatus.OK);
+    }
+
+    @GetMapping("/getByName")
+    public ResponseEntity<?> getByName(@RequestParam(required = false) String firstname,
+                                       @RequestParam(required = false) String lastname) {
+
+        // validation: no numbers allowed
+        if ((firstname != null && firstname.matches(".*\\d.*")) ||
+                (lastname != null && lastname.matches(".*\\d.*"))) {
+            return ResponseEntity.badRequest().body("Firstname/Lastname should not contain numbers.");
+        }
+
+        List<Resident> residents = residentService.findByName(firstname, lastname);
+
+        if (residents.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("First name OR last name is required...");
+        }
+        return ResponseEntity.ok(residents);
+    }
+}
+
+
+
+
