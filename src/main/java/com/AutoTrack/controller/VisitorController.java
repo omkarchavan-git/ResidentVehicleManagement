e94e0f97ef2bc8b331f483ca0ed4b873c91e8a49
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-09-20 13:06:34 +0530

    Updated

--- src/main/java/com/AutoTrack/controller/VisitorController.java
+++ src/main/java/com/AutoTrack/controller/VisitorController.java
@@ -2,7 +2,7 @@ package com.AutoTrack.controller;
 
 import com.AutoTrack.dtoClasses.VisitorResidentDTO;
 import com.AutoTrack.entity.Visitor;
-import com.AutoTrack.service.VisitorService;
+import com.AutoTrack.serviceImpl.VisitorServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,20 +19,20 @@ import java.util.Map;
 public class VisitorController {
 
     @Autowired
-    private VisitorService visitorService;
+    private VisitorServiceImpl visitorServiceImpl;
 
 
     // API to add visitor data
     @PostMapping("/addVisitor")
     public ResponseEntity<Visitor> addVisitor(@RequestBody Visitor visitor) {
-        Visitor savedVisitor = visitorService.addVisitor(visitor);
+        Visitor savedVisitor = visitorServiceImpl.addVisitor(visitor);
         return new ResponseEntity<>(savedVisitor, HttpStatus.CREATED);
     }
 
     // controller to get visitor details by regNum
     @GetMapping("/resident/{regNum}")
     public ResponseEntity<VisitorResidentDTO> getVisitorResidentDetails(@PathVariable String regNum) {
-        VisitorResidentDTO details = visitorService.getVisitorResidentDetailsByRegNum(regNum);
+        VisitorResidentDTO details = visitorServiceImpl.getVisitorResidentDetailsByRegNum(regNum);
         return ResponseEntity.ok(details);
     }
 
@@ -43,7 +43,7 @@ public class VisitorController {
             @RequestBody Map<String, String> requestBody) {
 
         LocalDateTime exitTime = LocalDateTime.parse(requestBody.get("timeOut"));
-        VisitorResidentDTO updatedVisitor = visitorService.updateVisitorExitTime(regNum, exitTime);
+        VisitorResidentDTO updatedVisitor = visitorServiceImpl.updateVisitorExitTime(regNum, exitTime);
 
         return ResponseEntity.ok(updatedVisitor);
     }
@@ -54,7 +54,7 @@ public class VisitorController {
     public ResponseEntity<List<VisitorResidentDTO>> getVisitorsByFilter(
             @RequestParam(required = false) List<String> types) {
 
-        List<VisitorResidentDTO> visitors = visitorService.getVisitorsByFilter(types);
+        List<VisitorResidentDTO> visitors = visitorServiceImpl.getVisitorsByFilter(types);
         return ResponseEntity.ok(visitors);
     }
 
--- src/main/java/com/AutoTrack/entity/Visitor.java
+++ src/main/java/com/AutoTrack/entity/Visitor.java
@@ -10,7 +10,6 @@ import jakarta.validation.constraints.Pattern;
 import java.time.Duration;
 import java.time.LocalDateTime;
 
-
 @Entity
 
 public class Visitor {
