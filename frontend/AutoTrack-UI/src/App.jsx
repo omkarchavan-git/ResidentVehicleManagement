06b8fbb361c6a5261fe64356d5aaeb57c505007a
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-09-22 00:10:58 +0530

    added Dashboard

--- frontend/AutoTrack-UI/src/App.jsx
+++ frontend/AutoTrack-UI/src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
-import Resident from './addResident/Resident'
-import Visitor from './addVisitor/Visitor'
-import Navbar from './navbar/Navbar'
+import { useState } from 'react';
+import { Navigate } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Resident from './addResident/Resident';
+import Visitor from './addVisitor/Visitor';
+import Navbar from './navbar/Navbar';
+import HomeDashboard from './Dashboard/HomeDashboard';
+import AddResident from './addResident/AddResident';
 
+import './App.css';
 
-import './App.css'
-import AddResident from './addResident/AddResident'
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -17,16 +19,18 @@ function App() {
           <Navbar />
           <div className="pt-16"> {/* padding so content doesn’t hide behind navbar */}
             <Routes>
+              <Route path='/' element={<Navigate to="/Dashboard" />} />
+              <Route path='/Dashboard' element={<HomeDashboard />} />
               <Route path="/resident/addResident" element={<AddResident />} />
               <Route path="/resident/Resident" element={<Resident />} />
               <Route path="/visitor" element={<Visitor />} />
-              {/* <Route path="/vehicle" element={<Vehicle />} /> */}
             </Routes>
+
           </div>
         </Router>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
