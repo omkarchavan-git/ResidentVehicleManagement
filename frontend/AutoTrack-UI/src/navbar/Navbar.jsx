c39693e9b55afc832bd38c748ee9f9a6f122c6f9
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-09-21 16:16:00 +0530

    added dropdown options

--- frontend/AutoTrack-UI/src/navbar/Navbar.jsx
+++ frontend/AutoTrack-UI/src/navbar/Navbar.jsx
@@ -1,16 +1,29 @@
 import { Link } from "react-router-dom";
-import "./Navbar.css"; // import css
+import "./Navbar.css";
 
 function Navbar() {
   return (
     <nav className="navbar">
       <div className="navbar-container">
         {/* Logo */}
-        <div className="logo">Vehicle Management</div>
+        <div className="logo">
+          <Link to="/" className="nav-item">Vehicle Management</Link>
+        </div>
 
         {/* Navigation Links */}
         <div className="nav-links">
-          <Link to="/resident" className="nav-item">Resident</Link>
+          {/* Resident with Dropdown */}
+          <div className="dropdown">
+            <Link to="/resident" className="nav-item">Resident</Link>
+            <div className="dropdown-content">
+              <Link to="./resident/addResident">Add Resident</Link>
+              <Link to="/resident/list">Resident List</Link>
+              <Link to="/resident/payments">Payments</Link>
+              <Link to="/resident/settings">Settings</Link>
+            </div>
+          </div>
+
+          {/* Normal links */}
           <Link to="/visitor" className="nav-item">Visitor</Link>
           <Link to="/vehicle" className="nav-item">Vehicle</Link>
         </div>
