985f0919a3a525e82c18e620a34b7086a399283e
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-09-22 00:07:53 +0530

    added method to get recent data

--- src/main/java/com/AutoTrack/repository/VehicleRepo.java
+++ src/main/java/com/AutoTrack/repository/VehicleRepo.java
@@ -3,8 +3,15 @@ package com.AutoTrack.repository;
 import com.AutoTrack.entity.Vehicles;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface VehicleRepo extends JpaRepository<Vehicles, Integer> {
     Optional<Vehicles> findByRegNum(String regNum);
+
+    long count(); // total vehicles
+    long countByIsVehActive(boolean isVehActive); // active vehicles
+    List<Vehicles> findTop5ByOrderByCreatedAtDesc(); // recent vehicles
+
+
 }
