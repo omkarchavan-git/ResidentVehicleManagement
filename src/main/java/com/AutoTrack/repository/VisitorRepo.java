7ea126c3756238b9c3fb1e9feecd203390032ab0
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-09-22 00:08:05 +0530

    added method to get recent data

--- src/main/java/com/AutoTrack/repository/VisitorRepo.java
+++ src/main/java/com/AutoTrack/repository/VisitorRepo.java
@@ -5,6 +5,8 @@ import com.AutoTrack.entity.Visitor;
 import jakarta.validation.constraints.Null;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,5 +15,9 @@ public interface VisitorRepo extends JpaRepository<Visitor, Integer> {
     Optional<Visitor> findByVehicalRegisterationNum(String vehicalRegisterationNum);
 
     List<Visitor> findByVisitorTypeIn(List<VisitorType> visitorTypes);
+
+    long countByCreatedAtBetween(LocalDateTime start, LocalDateTime end);
+
+    List<Visitor> findTop5ByOrderByCreatedAtDesc();
 
 }
