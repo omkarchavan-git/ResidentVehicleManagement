f08f38f45f8639f1b87f53084c2f14338a6420cb
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-09-24 18:52:25 +0530

    added exportToPdf Functionality

--- frontend/AutoTrack-UI/src/components/navbar/Navbar.jsx
+++ frontend/AutoTrack-UI/src/components/navbar/Navbar.jsx
@@ -1,29 +1,40 @@
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
-function Navbar() {
+function Navbar({ residentList, exportToPDF }) {
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        {/* Logo */}
         <div className="logo">
           <Link to="/" className="nav-item">Vehicle Management</Link>
         </div>
 
-        {/* Navigation Links */}
         <div className="nav-links">
-          {/* Resident with Dropdown */}
           <div className="dropdown">
             <Link to="/resident/Resident" className="nav-item">Resident</Link>
             <div className="dropdown-content">
               <Link to="/resident/AddResident">Add Resident</Link>
-              <Link to="/resident/list">Update Resident</Link>
+              <div style={{ marginBottom: "10px" }}>
+                <button
+                  onClick={exportToPDF}
+                  style={{
+                    padding: "6px 12px",
+                    marginRight: "10px",
+                    background: "#e65100",
+                    color: "#fff",
+                    border: "none",
+                    cursor: "pointer"
+                  }}
+                >
+                  Export to PDF
+                </button>
+              </div>
+
               <Link to="/resident/payments">Payments</Link>
               <Link to="/resident/settings">Settings</Link>
             </div>
           </div>
 
-          {/* Normal links */}
           <Link to="/visitor" className="nav-item">Visitor</Link>
           <Link to="/vehicle" className="nav-item">Vehicle</Link>
         </div>
