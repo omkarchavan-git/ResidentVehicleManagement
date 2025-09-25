02a4c44e399ab40cbd9dcb9a04b39ba18647cb31
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-09-25 17:44:16 +0530

    Implemented method to get all visitors data

--- src/main/java/com/AutoTrack/service/VisitorService.java
+++ src/main/java/com/AutoTrack/service/VisitorService.java
@@ -1,7 +1,32 @@
 package com.AutoTrack.Service;
 
+import com.AutoTrack.dtoClasses.VisitorResidentDTO;
+import com.AutoTrack.entity.Visitor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface VisitorService {
 
+    public Visitor addVisitor(Visitor visitor);
+
+    // method to get Resident details using visitor regNum
+    public VisitorResidentDTO getVisitorResidentDetailsByRegNum(String regNum);
+
+    // method to update timeout of visitor
+    public VisitorResidentDTO updateVisitorExitTime(String regNum, LocalDateTime exitTime);
+
+    // method to get visitor by their Type
+    public List<VisitorResidentDTO> getVisitorsByFilter(List<String> types);
+
+    // updated method to add outTime and update the Duration Hours
+    // Update timeOut and auto-calculate visitDuration
+    public Visitor updateVisitorExit(String vehicalRegisterationNum, LocalDateTime timeOut);
+
+    // get all visitor
+    public List<Visitor> getAllVisitor();
+
+    }
 
 
-}
+
