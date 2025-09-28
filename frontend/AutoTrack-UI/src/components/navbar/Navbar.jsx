bc8c06af676770af680e3dd1e68c3b58f89f72ad
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-09-28 23:18:18 +0530

    added logo image

--- frontend/AutoTrack-UI/src/components/navbar/Navbar.jsx
+++ frontend/AutoTrack-UI/src/components/navbar/Navbar.jsx
@@ -3,28 +3,38 @@ import "./Navbar.css";
 
 function Navbar({ residentList, exportToPDF }) {
   return (
-    <nav className="navbar">
-      <div className="navbar-container">
-        <div className="logo">
-          <Link to="/" className="nav-item">Vehicle Management</Link>
-        </div>
+    <>
+       
+      <div className="logo-wrapper">
+        <img
+          src="./images/logo.png"
+          alt="Logo"
+          className="logo-image"
+        />
+      </div>
 
-        <div className="nav-links">
-          <div className="dropdown">
-            <Link to="/resident/Resident" className="nav-item">Resident</Link>
-            <div className="dropdown-content">
-              <Link to="/resident/AddResident">Add Resident</Link>
-
-              <Link to="/resident/payments">Payments</Link>
-              <Link to="/resident/settings">Settings</Link>
-            </div>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <div className="logo-text">
+            <Link to="/" className="nav-item">Vehicle Management</Link>
           </div>
 
-          <Link to="/visitor" className="nav-item">Visitor</Link>
-          <Link to="/vehicle" className="nav-item">Vehicle</Link>
+          <div className="nav-links">
+            <div className="dropdown">
+              <Link to="/resident/Resident" className="nav-item">Resident</Link>
+              <div className="dropdown-content">
+                <Link to="/resident/AddResident">Add Resident</Link>
+                <Link to="/resident/payments">Payments</Link>
+                <Link to="/resident/settings">Settings</Link>
+              </div>
+            </div>
+
+            <Link to="/visitor" className="nav-item">Visitor</Link>
+            <Link to="/vehicle" className="nav-item">Vehicle</Link>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 }
 
