d8547ea59260b448c68c06ae5de76e87fa853a14
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-09-29 13:59:04 +0530

    Implemented method to getallvehicles data method

--- src/main/java/com/AutoTrack/serviceImpl/VehicleServiceImpl.java
+++ src/main/java/com/AutoTrack/serviceImpl/VehicleServiceImpl.java
@@ -1,5 +1,6 @@
 package com.AutoTrack.serviceImpl;
 
+import com.AutoTrack.Service.VehicleService;
 import com.AutoTrack.entity.Resident;
 import com.AutoTrack.entity.Vehicles;
 import com.AutoTrack.repository.ResidentRepo;
@@ -8,9 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
-public class VehicleServiceImpl {
+public class VehicleServiceImpl implements VehicleService {
 
     @Autowired
     private VehicleRepo vehiclrRepo;
@@ -18,32 +20,46 @@ public class VehicleServiceImpl {
     @Autowired
     private ResidentRepo residentRepo;
 
-    // method to add vehicle with resident
-    public Vehicles createVehicle (Vehicles vehicles, int residentId)
-    {
-        // check if resident  exist
-
-        Resident resident =  residentRepo.findById(residentId)
-                .orElseThrow( () -> new IllegalArgumentException("Resident not found with id : " + residentId));
-
-        // validate reg num
-        if (vehicles.getRegNum() == null || vehicles.getRegNum().trim().isEmpty())
-        {
-            throw new IllegalArgumentException("Vehicle reg num is not valid");
-        }
-
-        // set resident to vehicle
-        vehicles.setResident(resident);
-        vehicles.setIntime(LocalDateTime.now().withNano(0));
-
-        return vehiclrRepo.save(vehicles);
+    @Override
+    public List<Vehicles> getallvehiclesdata(List<Vehicles> vehiclesList) {
+        List<Vehicles> vehiclesList1 = vehiclrRepo.findAll();
+        return vehiclesList1;
     }
-    //
+
+    @Override
     public Resident getResidentByRegNum(String regNum) {
+
         Vehicles vehicle = vehiclrRepo.findByRegNum(regNum)
                 .orElseThrow(() -> new RuntimeException("Vehicle not found with registration number: " + regNum));
-        // ✅ Step 3: Return only resident details
+
         return vehicle.getResident();
     }
+
+
+    @Override
+    public Vehicles createVehicle(Vehicles vehicles, int residentId) {
+
+            Resident resident =  residentRepo.findById(residentId)
+                    .orElseThrow( () -> new IllegalArgumentException("Resident not found with id : " + residentId));
+
+            // validate reg num
+            if (vehicles.getRegNum() == null || vehicles.getRegNum().trim().isEmpty())
+            {
+                throw new IllegalArgumentException("Vehicle reg num is not valid");
+            }
+
+            // set resident to vehicle
+            vehicles.setResident(resident);
+            vehicles.setIntime(LocalDateTime.now().withNano(0));
+
+            return vehiclrRepo.save(vehicles);
+
+    }
+
+
+
+
+
+
 
 }
