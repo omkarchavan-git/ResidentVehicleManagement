bf03a0a9c291a490c7611487e118e5e995807bba
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-09-29 14:01:28 +0530

    implemented method to get all vehicles data

--- src/main/java/com/AutoTrack/service/VehicleService.java
+++ src/main/java/com/AutoTrack/service/VehicleService.java
@@ -1,4 +1,20 @@
 package com.AutoTrack.Service;
 
-public interface VehicleService {
+import com.AutoTrack.entity.Resident;
+import com.AutoTrack.entity.Vehicles;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface VehicleService  {
+
+
+    //method to get all vehicle data
+    public List<Vehicles> getallvehiclesdata(List<Vehicles> vehiclesList);
+
+    public Resident getResidentByRegNum(String regNum);
+
+    // method to add vehicle with resident
+    public Vehicles createVehicle (Vehicles vehicles, int residentId);
 }
