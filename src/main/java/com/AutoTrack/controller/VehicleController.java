f44c587c79cc2401ff8218c572aebc853e7f49c8
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-09-29 13:58:49 +0530

    Implemented API to getallvehicles data method

--- src/main/java/com/AutoTrack/controller/VehicleController.java
+++ src/main/java/com/AutoTrack/controller/VehicleController.java
@@ -1,20 +1,22 @@
 package com.AutoTrack.controller;
 
+import com.AutoTrack.Service.VehicleService;
 import com.AutoTrack.entity.Resident;
 import com.AutoTrack.entity.Vehicles;
-import com.AutoTrack.serviceImpl.VehicleServiceImpl;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/vehicle")
 public class VehicleController {
 
     @Autowired
-    private VehicleServiceImpl vehicleServiceImpl;
+    private VehicleService vehicleService;
 
     // API to add vehicle data with resident
     @PostMapping("/addVehicle")
@@ -22,18 +24,27 @@ public class VehicleController {
                                          @RequestParam int residentId)
     {
         try {
-            Vehicles savedVehicle = vehicleServiceImpl.createVehicle(vehicles, residentId);
+            Vehicles savedVehicle = vehicleService.createVehicle(vehicles, residentId);
             return new ResponseEntity<>(savedVehicle, HttpStatus.CREATED);
         } catch (IllegalArgumentException e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
     }
 
+    // API to get alll vehicles data
+    @GetMapping("/getallVehicles")
+    public ResponseEntity<List<Vehicles>> getallvehicles(List<Vehicles> vehiclesList)
+    {
+       List<Vehicles> vehiclesList1 =  vehicleService.getallvehiclesdata(vehiclesList);
+       return new ResponseEntity<>(vehiclesList1, HttpStatus.OK);
+
+    }
+
     // API to get Resident details by Vehicle Registration Number
     @GetMapping("/getResidentByRegNum")
     public ResponseEntity<?> getResidentByRegNum(@RequestParam String regNum) {
         try {
-            Resident resident = vehicleServiceImpl.getResidentByRegNum(regNum);
+            Resident resident = vehicleService.getResidentByRegNum(regNum);
             return ResponseEntity.ok(resident);         //  Only Resident details
         } catch (IllegalArgumentException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
