b0aea8253cb7b1e446b272ee12c9c91cafb86c0c
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-10-11 22:56:58 +0530

    added hamburger menu

--- frontend/AutoTrack-UI/src/components/navbar/Navbar.jsx
+++ frontend/AutoTrack-UI/src/components/navbar/Navbar.jsx
@@ -1,31 +1,70 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { useState } from "react";
 import "./Navbar.css";
 
-function Navbar({ residentList, exportToPDF }) {
+function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const location = useLocation(); //  to highlight active route
+
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+
+  const handleLinkClick = () => setIsMenuOpen(false);
+
   return (
     <>
       <nav className="navbar">
         <div className="navbar-container">
-          <div className="logo-text">
+          {/*  Logo Section */}
+          <Link to="/" className="logo-text" onClick={handleLinkClick}>
+            <div className="logo-wrapper">
+              <img
+                src="./images/logo2.png"
+                alt="Logo"
+                className="logo-image"
+              />
+              <p className="appname">Vehicle Management</p>
+            </div>
+          </Link>
 
-            <Link to="/" className="nav-item">
-              <div className="logo-wrapper">
-                <img
-                  src="./images/logo2.png"
-                  alt="Logo"
-                  className="logo-image"
-                />
-                <p className="appname">
-                  Vehicle Management
-                </p>
-              </div>
-            </Link>
+          {/*  Hamburger Icon */}
+          <div
+            className={`hamburger ${isMenuOpen ? "active" : ""}`}
+            onClick={toggleMenu}
+          >
+            <span></span>
+            <span></span>
+            <span></span>
           </div>
 
-          <div className="nav-links">
-            <Link to="/resident/Resident" className="nav-item"><p>Resident </p></Link>
-            <Link to="/visitor" className="nav-item"><p>Visitor </p></Link>
-            <Link to="/vehicle" className="nav-item"><p>Vehicle </p></Link>
+          {/*  Navigation Links */}
+          <div className={`nav-links ${isMenuOpen ? "show" : ""}`}>
+            <Link
+              to="/resident/Resident"
+              className={`nav-item ${
+                location.pathname === "/resident/Resident" ? "active" : ""
+              }`}
+              onClick={handleLinkClick}
+            >
+              Resident
+            </Link>
+            <Link
+              to="/visitor"
+              className={`nav-item ${
+                location.pathname === "/visitor" ? "active" : ""
+              }`}
+              onClick={handleLinkClick}
+            >
+              Visitor
+            </Link>
+            <Link
+              to="/vehicle"
+              className={`nav-item ${
+                location.pathname === "/vehicle" ? "active" : ""
+              }`}
+              onClick={handleLinkClick}
+            >
+              Vehicle
+            </Link>
           </div>
         </div>
       </nav>
