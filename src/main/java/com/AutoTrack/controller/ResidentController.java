255dc6a8cdd04c101ad14d9b4992b451e83c1b8c
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-10-21 17:35:18 +0530

    Updated CORS

--- src/main/java/com/AutoTrack/controller/ResidentController.java
+++ src/main/java/com/AutoTrack/controller/ResidentController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://resident-vehicle-managements.vercel.app/resident/Resident")
 @RequestMapping("/Resident")
 public class ResidentController {
 
