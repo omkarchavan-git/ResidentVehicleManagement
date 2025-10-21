d7ca7b97fbcef4e516b77bacabe3929c4f6b938b
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-10-21 22:16:09 +0530

    Updated PORT

--- src/main/java/com/AutoTrack/controller/HomeController.java
+++ src/main/java/com/AutoTrack/controller/HomeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "https://resident-vehicle-managements.vercel.app")
+@CrossOrigin(origins = "https://resident-vehicle-managements.vercel.app/")
 @RequestMapping("/home")
 
 public class HomeController {
