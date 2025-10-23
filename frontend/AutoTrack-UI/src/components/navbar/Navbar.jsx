a08cd05a66d9713f2c5a758b8181654bf2f09c91
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-10-23 12:40:15 +0530

    Residnet path updated

--- frontend/AutoTrack-UI/src/components/navbar/Navbar.jsx
+++ frontend/AutoTrack-UI/src/components/navbar/Navbar.jsx
@@ -1,75 +1,70 @@
 import { Link, useLocation } from "react-router-dom";
 import { useState } from "react";
 import "./Navbar.css";
-import logo from "../../assets/images/logo2.png"
-
+import logo from "../../assets/images/logo2.png";
 
 function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const location = useLocation(); //  to highlight active route
+  const location = useLocation(); // To highlight active route
 
   const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
-
   const handleLinkClick = () => setIsMenuOpen(false);
 
   return (
-    <>
-      <nav className="navbar">
-        <div className="navbar-container">
-          {/*  Logo Section */}
-          <Link to="/" className="logo-text" onClick={handleLinkClick}>
-            <div className="logo-wrapper">
+    <nav className="navbar">
+      <div className="navbar-container">
+        {/* Logo Section */}
+        <Link to="/" className="logo-text" onClick={handleLinkClick}>
+          <div className="logo-wrapper">
+            <img src={logo} alt="Logo" className="logo-image" />
+            <p className="appname">Vehicle Management</p>
+          </div>
+        </Link>
 
-             <img src={logo} alt="Logo" className="logo-image" />
+        {/* Hamburger Icon */}
+        <div
+          className={`hamburger ${isMenuOpen ? "active" : ""}`}
+          onClick={toggleMenu}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
 
-              <p className="appname"> Vehicle Management</p>
-            </div>
+        {/* Navigation Links */}
+        <div className={`nav-links ${isMenuOpen ? "show" : ""}`}>
+          <Link
+            to="/resident"
+            className={`nav-item ${
+              location.pathname === "/resident" ? "active" : ""
+            }`}
+            onClick={handleLinkClick}
+          >
+            <p className="navpage">Resident</p>
           </Link>
 
-          {/*  Hamburger Icon */}
-          <div
-            className={`hamburger ${isMenuOpen ? "active" : ""}`}
-            onClick={toggleMenu}
+          <Link
+            to="/visitor"
+            className={`nav-item ${
+              location.pathname === "/visitor" ? "active" : ""
+            }`}
+            onClick={handleLinkClick}
           >
-            <span></span>
-            <span></span>
-            <span></span>
-          </div>
+            <p className="navpage">Visitor</p>
+          </Link>
 
-          {/*  Navigation Links */}
-          <div className={`nav-links ${isMenuOpen ? "show" : ""}`}>
-            <Link
-              to="/resident/Resident"
-              className={`nav-item ${location.pathname === "/resident/Resident" ? "active" : ""
-                }`}
-              onClick={handleLinkClick}
-            >  <p className="navpage"> 
-                Resident
-              </p>
-            </Link>
-            <Link
-              to="/visitor"
-              className={`nav-item ${location.pathname === "/visitor" ? "active" : ""
-                }`}
-              onClick={handleLinkClick}
-            > <p>
-                Visitor
-              </p>
-            </Link>
-            <Link
-              to="/vehicle"
-              className={`nav-item ${location.pathname === "/vehicle" ? "active" : ""
-                }`}
-              onClick={handleLinkClick}
-            > <p>
-
-                Vehicle
-              </p>
-            </Link>
-          </div>
+          <Link
+            to="/vehicle"
+            className={`nav-item ${
+              location.pathname === "/vehicle" ? "active" : ""
+            }`}
+            onClick={handleLinkClick}
+          >
+            <p className="navpage">Vehicle</p>
+          </Link>
         </div>
-      </nav>
-    </>
+      </div>
+    </nav>
   );
 }
 
