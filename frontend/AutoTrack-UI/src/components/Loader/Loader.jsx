66569607edf47a606f3eae8e8a6d2e0dcc63207e
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-10-24 23:41:54 +0530

    added the quotes

---
+++ frontend/AutoTrack-UI/src/components/Loader/Loader.jsx
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from "react";
+import "./Loader.css";
+
+const quotes = [
+  "Good things take time — please wait!",
+  "Loading your dashboard... Excellence in progress!",
+  "Patience is the key to success!",
+  "Just a moment — setting up your community data!",
+  "Fetching residents, vehicles, and visitors — almost there!"
+];
