ba38746930368010eb8e669f03c14d6b71cfb5de
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-11-22 23:05:06 +0530

    updated routing path

--- frontend/AutoTrack-UI/src/components/Dashboard/HomeDashboard.jsx
+++ frontend/AutoTrack-UI/src/components/Dashboard/HomeDashboard.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import Loader from "../Loader/Loader"; // ✅ Make sure path is correct
+import Loader from "../Loader/Loader"; 
 import "./HomeDashboard.css";
 
 function HomeDashboard() {
