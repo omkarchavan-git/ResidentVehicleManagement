6a6ba1cdc7dab3d3ce7fa20d8ef0e11046fb9828
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-11-22 23:22:46 +0530

    added required input fileds

--- frontend/AutoTrack-UI/src/components/Login/Loginpage.jsx
+++ frontend/AutoTrack-UI/src/components/Login/Loginpage.jsx
@@ -4,6 +4,19 @@ function Loginpage () {
         <>
         <div>
             <h3>Login page</h3>
+            <div className="card">
+                <h1>Login</h1>
+
+                
+                <div class="login-form-group">
+                  <label for="password">User Name : </label>
+                  <input type="text" class="form-control" name="username" id="" aria-describedby="helpId" placeholder="username" />
+                 
+                  <label for="password">Password : </label>
+                  <input type="text" class="form-control" name="password" id="" aria-describedby="helpId" placeholder="password" />
+                 
+                </div>
+            </div>
         </div>
         </>
     );
